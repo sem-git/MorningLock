a073a0aca9166c29c418dcd7311a70fa0f8a7fc1
kwonseokki <kwons3327@gmail.com>
2025-12-20 04:52:31 +0900

    feat: 알람 시간 비교를 위한 Date extension에 테스트용 메서드 추가

--- WakeUp/Extension/Date+.swift
+++ WakeUp/Extension/Date+.swift
@@ -8,7 +8,26 @@
 import Foundation
 
 extension Date {
-    var getTime: Date {
+    var nextOccurrenceIncludingSeconds: Date {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.hour, .minute, .second], from: self)
+        var todayComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
+        todayComponents.hour = dateComponents.hour
+        todayComponents.minute = dateComponents.minute
+        todayComponents.second = dateComponents.second
+        
+        let today = calendar.date(from: todayComponents)!
+        let now = Date()
+        
+        // 시간이 지난 경우 다음 날짜로 설정
+        if today <= now {
+            return calendar.date(byAdding: .day, value: 1, to: today)!
+        } else {
+            return today
+        }
+    }
+    
+    var nextOccurrenceIncludingMinutes: Date {
         let calendar = Calendar.current
         let dateComponents = calendar.dateComponents([.hour, .minute], from: self)
         var todayComponents = calendar.dateComponents([.year, .month, .day], from: Date())
