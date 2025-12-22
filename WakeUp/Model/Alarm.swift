310e1becf161a295c19c87ba795227deeb45229e
sem-git <lsmsm0327@naver.com>
2025-12-22 21:03:13 +0900

    fix: 오전/오후 및 요일 한글 표기로 변경

--- WakeUp/Model/Alarm.swift
+++ WakeUp/Model/Alarm.swift
@@ -25,7 +25,7 @@ struct AlarmEntity: Hashable, Identifiable {
     
     var meridiem: String {
         let formatter = DateFormatter()
-        formatter.locale = .current
+        formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "a"
         let meridiem = formatter.string(from: fireDate)
         return meridiem
