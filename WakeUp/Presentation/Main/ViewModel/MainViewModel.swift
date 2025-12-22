3feb5de4dfb065a1f6afcd832b0d474b62ba3e05
kwonseokki <kwons3327@gmail.com>
2025-12-22 21:58:04 +0900

    fix: 미루기버튼 비활성화 처리

--- WakeUp/Presentation/Main/ViewModel/MainViewModel.swift
+++ WakeUp/Presentation/Main/ViewModel/MainViewModel.swift
@@ -56,12 +56,12 @@ class MainViewModel: ObservableObject {
                         
         // 알람이 울리는 중이거나 스누즈 횟수가 3회이상 초과시 버튼 disable
         Publishers.CombineLatest(
-            alarmManager.$isAlarmPlaying,
+            alarmManager.$isSnoozeActive,
             alarmManager.$snoozeCount
         )
         .receive(on: RunLoop.main)
-        .map { isAlarmPlaying, snoozeCount in
-            !isAlarmPlaying || snoozeCount >= 3
+        .map { isSnoozeActive, snoozeCount in
+            isSnoozeActive || snoozeCount >= 3
         }
         .assign(to: \.snoozeDisabled, on: self)
         .store(in: &cancellables)        
