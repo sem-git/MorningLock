36d5c2a584af09879bd40acbb4c95c9660364cd3
kwonseokki <kwons3327@gmail.com>
2025-12-29 19:51:14 +0900

    fix: 빌드 설정에 따라 AdMob 광고 ID 분기 처리

--- WakeUp/Sources/Presentation/Ads/ViewModel/NativeAdViewModel.swift
+++ WakeUp/Sources/Presentation/Ads/ViewModel/NativeAdViewModel.swift
@@ -9,30 +9,37 @@ import Combine
 import GoogleMobileAds
 
 class NativeAdViewModel: NSObject, ObservableObject, NativeAdLoaderDelegate, NativeAdDelegate {
-  @Published var nativeAd: NativeAd?
-  private var adLoader: AdLoader!
+    @Published var nativeAd: NativeAd?
+    
+    private var adLoader: AdLoader!
+    
+    var isLoading: Bool {
+        adLoader.isLoading
+    }
     
     override init() {
         super.init()
         self.refreshAd()
     }
-
-  func refreshAd() {
-    adLoader = AdLoader(
-      adUnitID: "ca-app-pub-3940256099942544/3986624511",
-      rootViewController: nil,
-      adTypes: [.native], options: nil)
-    adLoader.delegate = self
-    adLoader.load(Request())
-  }
-
-  func adLoader(_ adLoader: AdLoader, didReceive nativeAd: NativeAd) {
-    // Native ad data changes are published to its subscribers.
-    self.nativeAd = nativeAd
-    nativeAd.delegate = self
-  }
-
-  func adLoader(_ adLoader: AdLoader, didFailToReceiveAdWithError error: Error) {
-    print("\(adLoader) failed with error: \(error.localizedDescription)")
-  }
+    
+    func refreshAd() {
+        guard let adUnitID = Bundle.main.infoDictionary?["AdUnitID"] as? String else { return }
+        
+        adLoader = AdLoader(
+            adUnitID: adUnitID,
+            rootViewController: nil,
+            adTypes: [.native], options: nil)
+        adLoader.delegate = self
+        adLoader.load(Request())
+    }
+    
+    func adLoader(_ adLoader: AdLoader, didReceive nativeAd: NativeAd) {
+        // Native ad data changes are published to its subscribers.
+        self.nativeAd = nativeAd
+        nativeAd.delegate = self
+    }
+    
+    func adLoader(_ adLoader: AdLoader, didFailToReceiveAdWithError error: Error) {
+        print("\(adLoader) failed with error: \(error.localizedDescription)")
+    }
 }
