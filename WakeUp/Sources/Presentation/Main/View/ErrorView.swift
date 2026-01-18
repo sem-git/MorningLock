afca0176a7628be525b2bc28d52c5f99b2bf0ac9
sem-git <lsmsm0327@naver.com>
2026-01-19 01:21:47 +0900

    feat: ErrorView 구현

---
+++ WakeUp/Sources/Presentation/Main/View/ErrorView.swift
@@ -0,0 +1,52 @@
+//
+//  ErrorView.swift
+//  WakeUp
+//
+//  Created by 이세민 on 1/19/26.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    @State private var isContactFormPresented = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 0) {
+                Text("오류가 발생했습니다\n잠시 후 다시 시도해주세요")
+                    .bold22()
+                    .padding(.top, 50)
+                    .multilineTextAlignment(.center)
+                
+                Text("오류가 계속된다면\n오른쪽 위의 ‘문의’를 통해 알려주세요.")
+                    .semiBold17(color: .gray200)
+                    .padding(.top, 12)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                MainButton(title: String(localized: "다시 시도하기"))
+            }
+            .padding(.horizontal, 16)
+            .navigationBarItems(trailing: contactButton)
+            .sheet(isPresented: $isContactFormPresented) {
+                WebView(
+                    url: "https://docs.google.com/forms/d/e/1FAIpQLSduOHAV4hz962dKI66QEk8KmBkxgmQaT7hFD8xJQgCX4TQr8w/viewform?usp=dialog"
+                )
+            }
+        }
+    }
+}
+
+// MARK: - SubViews
+
+extension ErrorView {
+    private var contactButton: some View {
+        Button {
+            isContactFormPresented.toggle()
+        } label: {
+            Text("문의")
+                .regular15()
+        }
+    }
+}
