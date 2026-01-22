ff8871ce1f9149d1c0559d2d0697f041ce22880c
sem-git <lsmsm0327@naver.com>
2026-01-22 21:10:50 +0900

    feat: 일정 시간 경과로 잠금 스킵 로직 구현

--- WakeUp/Sources/Presentation/Main/ViewModel/MainViewModel.swift
+++ WakeUp/Sources/Presentation/Main/ViewModel/MainViewModel.swift
@@ -36,6 +36,9 @@ class MainViewModel: ObservableObject {
     
     private var cancellables = Set<AnyCancellable>()
     
+    private var alarmTimeoutTask: Task<Void, Never>?
+    private let alarmMaxWaitingTime: TimeInterval = .minutes(20) // 1시간으로 변경 예정
+    
     init() {
         self.coreDataManager = .shared
         self.alarmManager = .shared
@@ -52,7 +55,16 @@ class MainViewModel: ObservableObject {
         // 알람이 재생 중이면서 isOpenSheet 보임 여부에 따라서 Sheet 열기
         alarmManager.$isAlarmPlaying
             .receive(on: RunLoop.main)
-            .assign(to: \.isAlarmSheetPresented, on: self)
+            .sink { [weak self] isPlaying in
+                guard let self else { return }
+                self.isAlarmSheetPresented = isPlaying
+                
+                if isPlaying {
+                    self.startAlarmTimeout()
+                } else {
+                    self.cancelAlarmTimeout()
+                }
+            }
             .store(in: &cancellables)
         
         // 스누즈 횟수
@@ -160,5 +172,33 @@ class MainViewModel: ObservableObject {
         }
         
         await storeKitManager.purchase(product)
+    }
+    
+    private func startAlarmTimeout() {
+        cancelAlarmTimeout()
+        
+        alarmTimeoutTask = Task { [weak self] in
+            guard let self else { return }
+            
+            try? await Task.sleep(
+                nanoseconds: UInt64(alarmMaxWaitingTime * 1_000_000_000)
+            )
+            
+            if self.isAlarmSheetPresented {
+                self.deactiveAlarmWithoutLock()
+            }
+        }
+    }
+    
+    private func cancelAlarmTimeout() {
+        alarmTimeoutTask?.cancel()
+        alarmTimeoutTask = nil
+    }
+    
+    private func deactiveAlarmWithoutLock() {
+        alarmManager.deactiveAlarm()
+        
+        isAlarmSheetPresented = false
+        isLockSkipSheetPresented = true
     }
 }
